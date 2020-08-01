c5c34fbaf194b9f0c16e23b8052f6da33381cdd3
Adinda Pratiwi Prameswari <yagami.chira@gmail.com>
2020-08-01 18:40:31 +0700

    update

--- Pekan 2/Tugas-Hari-Ke-5/my-app/src/App.js
+++ Pekan 2/Tugas-Hari-Ke-5/my-app/src/App.js
@@ -8,15 +8,15 @@ function App() {
           <form action="">
             <table>
                   <tr><td>Nama Pelanggan</td>
-                  <td><input type="text" name="" id=""></td></tr>
+                  <td><input type="text" name="" id=""/></td></tr>
                   <tr><td>Daftar Item</td>
-                  <td><input type="checkbox" name="buah" id=""> <label for="">Semangka</label><br>
-                  <input type="checkbox" name="buah" id=""> <label for="">Jeruk</label><br>
-                  <input type="checkbox" name="buah" id=""> <label for="">Nanas</label><br>
-                  <input type="checkbox" name="buah" id=""> <label for="">Salak</label><br>
-                  <input type="checkbox" name="buah" id=""> <label for="">Anggur</label></td></tr>
+                  <td><input type="checkbox" name="buah" id=""/> <label for="">Semangka</label><br/>
+                  <input type="checkbox" name="buah" id=""/> <label for="">Jeruk</label><br/>
+                  <input type="checkbox" name="buah" id=""/> <label for="">Nanas</label><br/>
+                  <input type="checkbox" name="buah" id=""/> <label for="">Salak</label><br/>
+                  <input type="checkbox" name="buah" id=""/> <label for="">Anggur</label></td></tr>
             </table>
-            <input type="submit" value="Kirim">
+            <input type="submit" value="Kirim"/>
           </form>
     </div>
   );
